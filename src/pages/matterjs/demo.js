d9402cc704421bf4ea63ef25f97c7128bbb1240e
dashboard <khangluong410@gmail.com>
2022-10-11 17:01:44 +0700

    update

--- src/pages/matterjs/demo.js
+++ src/pages/matterjs/demo.js
@@ -1,12 +1,44 @@
 /* eslint-disable */
 import { useEffect, useRef, useState } from 'react';
 import Matter from 'matter-js';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import moment from 'moment';
+import { Line } from 'react-chartjs-2';
+import 'chartjs-plugin-streaming';
 import Page from '../../components/Page';
 
 // module aliases
+const Chart = require('react-chartjs-2').Chart;
+
+const chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(75, 192, 192)',
+  blue: 'rgb(54, 162, 235)',
+  purple: 'rgb(153, 102, 255)',
+  grey: 'rgb(201, 203, 207)',
+};
+
+const color = Chart.helpers.color;
+const data1 = {
+  datasets: [
+    {
+      label: 'Dataset 1 (linear interpolation)',
+      backgroundColor: color(chartColors.red).alpha(0.5).rgbString(),
+      borderColor: chartColors.red,
+      fill: false,
+      lineTension: 0,
+      borderDash: [8, 4],
+      data: [],
+    },
+  ],
+};
 
 export default function Demomatter() {
   const [vel, setVel] = useState({ x: 0, y: 0 });
+  const [fix, setFix] = useState({});
+
   var scene = useRef();
   var isPressed = useRef(false);
   var Engine = Matter.Engine,
@@ -35,10 +67,6 @@ export default function Demomatter() {
 
   useEffect(() => {
     World.add(engine.current.world, [body, constraint]);
-
-    var body1 = Bodies.rectangle(200, 200, 50, 50);
-
-    World.add(engine.current.world, body1);
 
     var render = Render.create({
       element: scene.current,
@@ -78,22 +106,74 @@ export default function Demomatter() {
       min: { x: 0, y: 0 },
       max: { x: 800, y: 600 },
     });
-    Events.on(engine.current, 'afterUpdate', () => setVel({ ...body.velocity }));
+
+    const interval = setInterval(() => {
+      setVel({ ...body.velocity });
+    }, 0);
+
+    Events.on(engine.current, 'afterUpdate', () => interval);
 
     Matter.Runner.run(engine.current);
     Render.run(render);
-  }, [setVel]);
+  }, []);
 
-  // useEffect(() => {
-  //   Events.on(engine.current, 'afterUpdate', () => setVel(body.velocity));
-  //   console.log(body.velocity);
-  // });
+  useEffect(() => {
+    setFix({
+      elements: {
+        line: {
+          tension: 0.5,
+        },
+      },
+      scales: {
+        xAxes: [
+          {
+            type: 'realtime',
+            distribution: 'linear',
+            realtime: {
+              onRefresh: function (chart) {
+                chart.data.datasets[0].data.push({
+                  x: moment(),
+                  y: vel.x,
+                });
+              },
+              time: {
+                displayFormat: 'h:mm',
+              },
+            },
+            ticks: {
+              displayFormats: 1,
+              maxRotation: 0,
+              minRotation: 0,
+              stepSize: 1,
+              maxTicksLimit: 30,
+              minUnit: 'second',
+              source: 'auto',
+              autoSkip: true,
+              callback: function (value) {
+                return moment(value, 'HH:mm:ss').format('mm:ss');
+              },
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              max: 20,
+              min: -20,
+            },
+          },
+        ],
+      },
+    });
+  }, [vel]);
 
   return (
     <Page>
       <div ref={scene} style={{ width: '100%', height: '100%' }} />
       x:{vel.x.toFixed(2).toString()}
       y:{vel.y.toFixed(2).toString()}
+      <Line data={data1} options={fix} />
     </Page>
   );
 }
